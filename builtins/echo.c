95f68b493e8397e99b4c9f4544704d751a9d9ecf
yassir elatmani <elatmaniyassir3@gmail.com>
2022-06-13 15:13:05 +0100

    echo builtin initial implementation

--- builtins/echo.c
+++ builtins/echo.c
@@ -4,7 +4,22 @@
 int main (int argc, char *argv[])
 {
     int i = 1;
+    char thrilling;
+
+    thrilling = '\0';
+    if (strcmp (argv[i], "-n"))
+        thrilling = '\n';
+    else
+        i++;
 
     while (argv[i])
-        printf ("|%s|\n", argv[i++]);
+    {
+        if (i == (argc - 1))
+            printf ("%s", argv[i]);
+        else
+            printf ("%s ", argv[i]);
+        i++;
+    }
+    printf ("%c", thrilling);
+    return (EXIT_SUCCESS);
 }
